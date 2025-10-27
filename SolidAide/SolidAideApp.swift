32b3fb8c26a1ac10ea98fcdac262c9b2a0515061
Flourdau <lourdaux.florian.pro@gmail.com>
2025-10-27 15:06:38 +0100

    First push, add .gitignire & xcode project

---
+++ SolidAide/SolidAideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidAideApp.swift
+//  SolidAide
+//
+//  Created by apprenant78 on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct SolidAideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
