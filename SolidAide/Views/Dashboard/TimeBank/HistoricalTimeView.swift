67a16dc6e0cb2baacde5e1af18b7fef789e3179d
Flourdau <lourdaux.florian.pro@gmail.com>
2025-11-06 19:40:19 +0100

    Debug chat & clean all 2! 🩷

---
+++ SolidAide/Views/Dashboard/TimeBank/HistoricalTimeView.swift
@@ -0,0 +1,16 @@
+//
+//  HistoricalTimeView.swift
+//  SolidAide
+//
+//  Created by Apprenant 78 on 31/10/2025.
+//
+
+import Foundation
+import SwiftData
+import SwiftUI
+
+struct HistoricalTimeView: View {
+    var body: some View {
+
+    }
+}
